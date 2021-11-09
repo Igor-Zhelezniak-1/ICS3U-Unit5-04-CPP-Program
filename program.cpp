c422ca43237100729d21cb8cdd9835ab77a4fe60
Igor <ihor.zhelezniak@mths.ca>
2021-11-09 16:39:39 +0000

    updated

--- program.cpp
+++ program.cpp
@@ -11,11 +11,14 @@ double volumeCalculator(float radius, float height) {
     // This is function for volume
     int volume;
     volume = pow(radius, 2) * height * M_PI;
+
     return volume;
 }
 
 
 main() {
+    // This is main function
+
     std::string integer1;
     std::string integer2;
     float radiusFromUser;
@@ -25,7 +28,7 @@ main() {
     std::cout << "Starting ..." << std::endl;
 
     std::cout << "" << std::endl;
-    std::cout << "This progaram calculates volume of a cylinder" << std::endl;
+    std::cout << "This program calculates volume of a cylinder" << std::endl;
     std::cout << "Please enter the radius and height" << std::endl;
 
     std::cout << "" << std::endl;
